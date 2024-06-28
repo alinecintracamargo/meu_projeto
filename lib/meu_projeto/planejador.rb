4d28e20a107624ef49731248e417d75089caf6c2
alinecintracamargo <alineandradecintra@gmail.com>
2024-06-28 12:46:35 -0300

    feat:

--- lib/meu_projeto/planejador.rb
+++ lib/meu_projeto/planejador.rb
@@ -7,12 +7,10 @@ require_relative 'participants'
 
 chosen_date_str = DateSuggester.ask_best_date
 
-
 event_date = Date.strptime(chosen_date_str, "%d/%m/%Y")
 
 
 puts "A data do evento foi escolhida: #{event_date.strftime("%d/%m/%Y")}"
-
 
 participants = [
   Participant.new("Família Camargo", "camargo@xxxxx.com"),
@@ -20,23 +18,16 @@ participants = [
   Participant.new("Família Cruz", "cruz@xxxxx.com")
 ]
 
-
 tasks = ["Lugar e Decoração", "Comida e Bebidas", "Músicas e Jogos"]
-
 
 task_organizer = TaskOrganizer.new(participants, tasks)
 
-
 task_organizer.choose_order
-
 
 task_organizer.organize_tasks
 
-
 task_organizer.update_task_status
 
-
 event_reminder = EventReminder.new(participants)
-
 
 event_reminder.send_reminders(event_date, 7)
